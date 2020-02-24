a511bd0622be6c1408e6172b7e9f24efa3091f7a
Tvovik <vovik142@ukr.net>
2020-02-24 12:13:07 +0000

    1`

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ btn.addEventListener('click', function(){
 let li = document.createElement('li');
 li.className += 'none';
 let input = document.querySelector('.info');
-li.innerHTML = input.value;
+li.innerHTML = input.value+'<span class="mark"> x</span>';
 input.value=""
 list.appendChild (li);
+li.addEventListener('click', function(){
+  this.style.color = "red";
 })
+let x = document.querySelector('.mark');
+x.addEventListener('click', function(){
+  li.parentNode.removeChild(li);
+});
+})
+
